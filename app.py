eb97f96957de21b8b0455f9192796e559e42f98c
Juan2040751 <viafara.juan@correounivalle.edu.co>
2024-05-24 23:05:32 -0500

    CV-21: first try

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 import boto3
+import json
+import time
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
@@ -13,7 +15,8 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_REGION = os.getenv('AWS_REGION')
 S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
-
+SNS_TOPIC_ARN = os.getenv('SNS_TOPIC_ARN')
+IAM_ROLE_ARN = os.getenv('IAM_ROLE_ARN')
 
 s3_client = boto3.client('s3', region_name=AWS_REGION,
                          aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -35,14 +38,30 @@ def upload_video():
     filename = secure_filename(file.filename)
     print('filename:', filename, file.filename)
     s3_client.upload_fileobj(file, S3_BUCKET_NAME, filename, ExtraArgs={'ContentType': 'video/webm'})
-
     response = rekognition_client.start_label_detection(
         Video={'S3Object': {'Bucket': S3_BUCKET_NAME, 'Name': filename}},
-        MinConfidence=50
+        MinConfidence=50,
+        NotificationChannel={
+            'SNSTopicArn': SNS_TOPIC_ARN,
+            'RoleArn': IAM_ROLE_ARN
+        }
     )
-    #falta getLabelDetection
-    return jsonify(response), 200
+    print("Start label detection response:", response)
+    job_id = response['JobId']
 
+
+    while True:
+        label_detection = rekognition_client.get_label_detection(JobId=job_id)
+        status = label_detection['JobStatus']
+        if status in ['SUCCEEDED', 'FAILED']:
+            break
+        time.sleep(1)
+
+    print("Label detection completed with status:", status)
+    if status == 'SUCCEEDED':
+        return jsonify(label_detection), 200
+    else:
+        return jsonify(label_detection), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
