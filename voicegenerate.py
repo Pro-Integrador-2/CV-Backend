b6a868acaae27c435e5d4add0d5b36a00fd2d259
Juan Jose Viafara <134220828+Jujo2040751@users.noreply.github.com>
2024-06-10 16:25:35 -0500

    CV-27: image (#21)
    * CV-43
    
    * CV-43: requirements
    
    * CV-43: websocket
    
    * CV-43: memoria
    
    * CV-36: Configuración para el despliegue.
    
    * CV-43: despliegue
    
    * CV-43: nuevos idiomas (#16)
    
    * Main (#17)
    
    * CV-43: websocket (#15)
    
    * CV-2: endpointapi
    
    * CV-2: start_label_detection
    
    * CV-21: first try
    
    * CV-21: Analicer
    
    * CV-21: frame-Rekognition
    
    * CV-21
    
    * procesamiento de etiquetas
    
    * CV-5: Notificación audible de objetos
    
    * CV-26: limpieza
    
    * CV-26: generate audio
    
    * CV-26: CV-38 creacion del endpoint
    
    * CV-23: Se parametrizó las traduciones de acuerdo al idioma.
    
    * CV-26: prompt improvement
    
    * CV-26: CV-39 integracion frontend
    
    * update prompt
    
    * CV-43
    
    * CV-43: requirements
    
    * CV-43: websocket
    
    * CV-43: memoria
    
    * CV-36: Configuración para el despliegue.
    
    * CV-43: despliegue
    
    ---------
    
    Co-authored-by: Deisy Melo <deisymelo0127@gmail.com>
    Co-authored-by: Andrés <borja.carlos@correounivalle.edu.co>
    Co-authored-by: Deisy05 <55037333+Deisy05@users.noreply.github.com>
    Co-authored-by: Juan David Castro <juan.david.castro@correounivalle.edu.co>
    Co-authored-by: JuanCardona97 <62678264+JuanCardona97@users.noreply.github.com>
    
    * Integracion sprint 4
    
    ---------
    
    Co-authored-by: Deisy Melo <deisymelo0127@gmail.com>
    Co-authored-by: Andrés <borja.carlos@correounivalle.edu.co>
    Co-authored-by: Deisy05 <55037333+Deisy05@users.noreply.github.com>
    Co-authored-by: Juan David Castro <juan.david.castro@correounivalle.edu.co>
    Co-authored-by: JuanCardona97 <62678264+JuanCardona97@users.noreply.github.com>
    
    * CV-27: cambio de la frecuencia de envio de imagenes
    
    * CV-27: cambio de la frecuencia de envio de imagenes en todos los idiomas
    
    * CV-27: 1 imagen
    
    ---------
    
    Co-authored-by: Juan David Castro <juan.david.castro@correounivalle.edu.co>
    Co-authored-by: JuanCardona97 <62678264+JuanCardona97@users.noreply.github.com>
    Co-authored-by: Deisy Melo <deisymelo0127@gmail.com>
    Co-authored-by: Andrés <borja.carlos@correounivalle.edu.co>
    Co-authored-by: Deisy05 <55037333+Deisy05@users.noreply.github.com>

--- voicegenerate.py
+++ voicegenerate.py
@@ -1,22 +1,23 @@
-import boto3
 import os
 import tempfile
-from dotenv import load_dotenv
 
-load_dotenv()
+import boto3
+
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
-def make_voice(text, language_code = "es"):
+
+def make_voice(text, language_code="es"):
     # Crear un cliente de Polly
     polly_client = boto3.client('polly', region_name='us-east-1',
                                 aws_access_key_id=AWS_ACCESS_KEY_ID,
                                 aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-
     voices = {
         'es': 'Lucia',  # Español
         'en': 'Joanna',  # Inglés
-        'fr': 'Celine'  # Francés
+        'fr': 'Celine',  # Francés
+        'pt': 'Vitoria',  # Portugués (Brasil)
+        'it': 'Carla'  # Italiano
     }
     voice_id = voices.get(language_code)
 
@@ -26,10 +27,9 @@ def make_voice(text, language_code = "es"):
         OutputFormat='mp3',
         VoiceId=voice_id
     )
-    
+
     # Guardar el audio en un archivo temporal
     with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as tmp_file:
         tmp_file_path = tmp_file.name
         tmp_file.write(response['AudioStream'].read())
-
     return tmp_file_path
