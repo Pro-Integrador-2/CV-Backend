32dabcf24c419361fc98b9068ba6515e3b465104
Juan Jose Viafara <134220828+Jujo2040751@users.noreply.github.com>
2024-06-12 11:00:46 -0500

    Mejoras sprint 5 (#30)
    * CV-27: image (#21)
    
    * CV-43
    
    * CV-43: requirements
    
    * CV-43: websocket
    
    * CV-43: memoria
    
    * CV-36: Configuración para el despliegue.
    
    * CV-43: despliegue
    
    * CV-43: nuevos idiomas (#16)
    
    * Main (#17)
    
    * CV-43: websocket (#15)
    
    * CV-2: endpointapi
    
    * CV-2: start_label_detection
    
    * CV-21: first try
    
    * CV-21: Analicer
    
    * CV-21: frame-Rekognition
    
    * CV-21
    
    * procesamiento de etiquetas
    
    * CV-5: Notificación audible de objetos
    
    * CV-26: limpieza
    
    * CV-26: generate audio
    
    * CV-26: CV-38 creacion del endpoint
    
    * CV-23: Se parametrizó las traduciones de acuerdo al idioma.
    
    * CV-26: prompt improvement
    
    * CV-26: CV-39 integracion frontend
    
    * update prompt
    
    * CV-43
    
    * CV-43: requirements
    
    * CV-43: websocket
    
    * CV-43: memoria
    
    * CV-36: Configuración para el despliegue.
    
    * CV-43: despliegue
    
    ---------
    
    Co-authored-by: Deisy Melo <deisymelo0127@gmail.com>
    Co-authored-by: Andrés <borja.carlos@correounivalle.edu.co>
    Co-authored-by: Deisy05 <55037333+Deisy05@users.noreply.github.com>
    Co-authored-by: Juan David Castro <juan.david.castro@correounivalle.edu.co>
    Co-authored-by: JuanCardona97 <62678264+JuanCardona97@users.noreply.github.com>
    
    * Integracion sprint 4
    
    ---------
    
    Co-authored-by: Deisy Melo <deisymelo0127@gmail.com>
    Co-authored-by: Andrés <borja.carlos@correounivalle.edu.co>
    Co-authored-by: Deisy05 <55037333+Deisy05@users.noreply.github.com>
    Co-authored-by: Juan David Castro <juan.david.castro@correounivalle.edu.co>
    Co-authored-by: JuanCardona97 <62678264+JuanCardona97@users.noreply.github.com>
    
    * CV-27: cambio de la frecuencia de envio de imagenes
    
    * CV-27: cambio de la frecuencia de envio de imagenes en todos los idiomas
    
    * CV-27: 1 imagen
    
    ---------
    
    Co-authored-by: Juan David Castro <juan.david.castro@correounivalle.edu.co>
    Co-authored-by: JuanCardona97 <62678264+JuanCardona97@users.noreply.github.com>
    Co-authored-by: Deisy Melo <deisymelo0127@gmail.com>
    Co-authored-by: Andrés <borja.carlos@correounivalle.edu.co>
    Co-authored-by: Deisy05 <55037333+Deisy05@users.noreply.github.com>
    
    * CV-28: Verificación de cambio significativo en el entorno
    
    * Cv 28 (#26)
    
    * CV-28: Verificación cambio significativo en el entorno
    
    * CV-28: ADD Labels gruops verify
    
    ---------
    
    Co-authored-by: anvorja <borja.carlos@correounivalle.edu.co>
    
    * Sprint 5: audio language
    
    * CV-28: Verificación similitu imagen antes de aws rekognition (#29)
    
    * CV-28: Verificación cambio significativo en el entorno
    
    * CV-28: ADD Labels gruops verify
    
    * Verificación similitu imagen antes de rekognition
    
    ---------
    
    Co-authored-by: Juan David Castro <juan.david.castro@correounivalle.edu.co>
    Co-authored-by: JuanCardona97 <62678264+JuanCardona97@users.noreply.github.com>
    Co-authored-by: Deisy Melo <deisymelo0127@gmail.com>
    Co-authored-by: Andrés <borja.carlos@correounivalle.edu.co>
    Co-authored-by: Deisy05 <55037333+Deisy05@users.noreply.github.com>

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from flask_socketio import SocketIO, emit
 from textgenerate import make_script, make_script_welcome
 from voicegenerate import make_voice
 from label_comparation import make_label_comparison
+from image_verify import compare_images
 
 app = Flask(__name__)
 CORS(app)
@@ -31,7 +32,7 @@ connections = {}
 @socketio.on('connect')
 def handle_connect():
     session_id = request.sid
-    connections[session_id] = {'last_detection': None, 'last_update': datetime.utcnow()}
+    connections[session_id] = {'last_image': None, 'last_update': datetime.utcnow()}
 
 
 @socketio.on('disconnect')
@@ -80,25 +81,25 @@ def handle_upload_image(data):
     try:
         image_bytes = base64.b64decode(image_data)
         if image_bytes:
-            labels = detect_labels_in_image(image_bytes)
-            currentDetection = make_script(labels, language)
+            
 
             current_time = datetime.utcnow()
             session_id = request.sid
 
             if session_id in connections:
-                last_detection = connections[session_id]['last_detection']
+                #last_detection = connections[session_id]['last_detection']
+                last_image = connections[session_id]['last_image']
                 last_update = connections[session_id]['last_update']
 
+                    
+                comparison_result = compare_images(last_image, image_bytes) if last_image else True
 
-                if last_detection:
-                    comparison_result = make_label_comparison(currentDetection, last_detection)
-
-                else:
-                    comparison_result = True
+                print (comparison_result)
 
                 if comparison_result or current_time - last_update > timedelta(minutes=1):
-                    connections[session_id]['last_detection'] = currentDetection
+                    labels = detect_labels_in_image(image_bytes)
+                    currentDetection = make_script(labels, language)
+                    connections[session_id]['last_image'] = image_bytes
                     connections[session_id]['last_update'] = current_time
 
                     audio_file_path = make_voice(currentDetection, language)
@@ -128,3 +129,134 @@ def handle_voice_guide(data):
 if __name__ == '__main__':
     socketio.run(app, debug=True)
 
+
+
+# import eventlet
+
+# eventlet.monkey_patch()
+
+# import base64
+# import json
+# import os
+# from datetime import datetime, timedelta
+
+# import boto3
+# from dotenv import load_dotenv
+# from flask import Flask, request
+# from flask_cors import CORS
+# from flask_socketio import SocketIO, emit
+
+# from textgenerate import make_script, make_script_welcome
+# from voicegenerate import make_voice
+# from label_comparation import make_label_comparison
+
+# app = Flask(__name__)
+# CORS(app)
+# socketio = SocketIO(app, cors_allowed_origins="*")
+
+# load_dotenv()
+# AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+# connections = {}
+
+
+# @socketio.on('connect')
+# def handle_connect():
+#     session_id = request.sid
+#     connections[session_id] = {'last_detection': None, 'last_update': datetime.utcnow()}
+
+
+# @socketio.on('disconnect')
+# def handle_disconnect():
+#     session_id = request.sid
+#     if session_id in connections:
+#         del connections[session_id]
+
+# def clean_labels(initialLabels):
+#     filtered_names = []
+
+#     for obj in initialLabels:
+#         if not obj['Parents'] and not any(category['Name'] == "Colors and Visual Composition" for category in obj['Categories']):
+#             filtered_names.append(obj['Name'])
+
+#     return filtered_names
+
+# def detect_labels_in_image(image_bytes):
+#     client = boto3.client('rekognition', region_name="us-east-1",
+#                           aws_access_key_id=AWS_ACCESS_KEY_ID,
+#                           aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+#     response = client.detect_labels(
+#         Image={'Bytes': image_bytes},
+#         MaxLabels=10,
+#         MinConfidence=75
+#     )
+#     labels = response['Labels']
+#     filtered_labels = clean_labels(labels)
+
+#     return json.dumps(filtered_labels)
+
+
+# @socketio.on('upload_image')
+# def handle_upload_image(data):
+#     global connections
+#     if not data or 'image' not in data:
+#         emit('error', {"error": "No image provided"})
+#         return
+
+#     image_data = data['image']
+#     language = data.get("language_code", "es")
+
+#     if image_data.startswith('data:image'):
+#         image_data = image_data.split(',')[1]
+
+#     try:
+#         image_bytes = base64.b64decode(image_data)
+#         if image_bytes:
+#             labels = detect_labels_in_image(image_bytes)
+#             currentDetection = make_script(labels, language)
+
+#             current_time = datetime.utcnow()
+#             session_id = request.sid
+
+#             if session_id in connections:
+#                 last_detection = connections[session_id]['last_detection']
+#                 last_update = connections[session_id]['last_update']
+
+
+#                 if last_detection:
+#                     comparison_result = make_label_comparison(currentDetection, last_detection)
+
+#                 else:
+#                     comparison_result = True
+
+#                 if comparison_result or current_time - last_update > timedelta(minutes=1):
+#                     connections[session_id]['last_detection'] = currentDetection
+#                     connections[session_id]['last_update'] = current_time
+
+#                     audio_file_path = make_voice(currentDetection, language)
+#                     with open(audio_file_path, 'rb') as f:
+#                         audio_data = f.read()
+#                     audio_base64 = base64.b64encode(audio_data).decode('utf-8')
+
+#                     emit('audio-detection', {'audio': audio_base64, 'session_id': session_id, "language": language})
+#     except Exception as e:
+#         emit('error', {"error": str(e)})
+
+
+# @socketio.on('voice_guide')
+# def handle_voice_guide(data):
+#     language = data.get("language_code", "es")
+#     try:
+#         welcome_text = make_script_welcome(language)
+#         audio_path = make_voice(welcome_text, language)
+#         with open(audio_path, 'rb') as f:
+#             audio_data = f.read()
+#         audio_base64 = base64.b64encode(audio_data).decode('utf-8')
+#         emit('audio-guide', {'audio': audio_base64})
+#     except Exception as e:
+#         emit('error', {"error-guide": str(e)})
+
+
+# if __name__ == '__main__':
+#     socketio.run(app, debug=True)
